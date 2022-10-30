dab830b995511813620df037429ac5b838d89989
YKMorsy <ykm3@pitt.edu>
2022-10-30 19:08:33 -0400

    Implemented set_switch_position() function

--- CTC_App/Iteration3/CTC.py
+++ CTC_App/Iteration3/CTC.py
@@ -107,8 +107,8 @@ class CTC:
         authority_list = self.__track_dictionary["track_block_authority"]
         for i in range(0, len(block_bool)):
             curr_block = block_bool[i]
-            line_num = (curr_block[0]-(curr_block[0]%1000))/1000 # translate line num (2000 + xyz -> 2)
-            block_num = curr_block[0] - (line_num*1000)
+            line_num = int((curr_block[0]-(curr_block[0]%1000))/1000) # translate line num (2000 + xyz -> 2)
+            block_num = int(curr_block[0] - (line_num*1000))
             block_status = curr_block[1]
 
             # get line and block index
@@ -121,6 +121,37 @@ class CTC:
         
         # update fault status and authority list in dictionary
         self.__track_dictionary["track_block_fault_status"] = fault_status_list
+        self.__track_dictionary["track_block_authority"] = authority_list
+
+    def set_switch_position(self, block_bool):
+        # get switch state list from dictionary
+        block_switches_list = self.__track_dictionary["track_block_switches"]
+        track_linked_list = self.__track_dictionary["track_block_linked_list"]
+        authority_list = self.__track_dictionary["track_block_authority"]
+        for i in range(0, len(block_bool)):
+            curr_block = block_bool[i]
+            line_num = int((curr_block[0]-(curr_block[0]%1000))/1000) # translate line num (2000 + xyz -> 2)
+            block_num = int(curr_block[0] - (line_num*1000))
+            switch_state = curr_block[1]
+
+            # get line and block index
+            line_index = self.__track_dictionary["track_id_external"].index(line_num)
+
+            # get block switches
+            block_switches = block_switches_list[line_index][block_num-1]
+
+            if switch_state == True:
+                track_linked_list[line_index][block_num] = max(block_switches[1], block_switches[0])
+                authority_list[line_index][min(block_switches[1], block_switches[0])] = False
+                authority_list[line_index][max(block_switches[1], block_switches[0])] = True
+            else:
+                track_linked_list[line_index][block_num] = min(block_switches[1], block_switches[0])
+                authority_list[line_index][max(block_switches[1], block_switches[0])] = False
+                authority_list[line_index][min(block_switches[1], block_switches[0])] = True
+
+        print(track_linked_list)
+        # update linked list and authority list in dictionary
+        self.__track_dictionary["track_block_linked_list"] = track_linked_list
         self.__track_dictionary["track_block_authority"] = authority_list
 
     # output is list of tuples -> [(x0,y0),(x1,y1),...,(xn,yn)] -> [(1001, 0), (1002, 1),...]
@@ -144,8 +175,9 @@ class CTC:
 ctc_test = CTC()
 ctc_test.add_track("Iteration3/Track_Layout_Blue.xlsx")
 ctc_test.set_fault_status([(1003, True)])
+ctc_test.set_switch_position([(1005, True)])
 authority_tuple_list = ctc_test.get_authority()
-# print(authority_tuple_list)
+print(authority_tuple_list)
 
 
         
