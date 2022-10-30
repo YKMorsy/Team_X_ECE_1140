368f630161e88a9f32101e40f79b501d277c4c15
YKMorsy <ykm3@pitt.edu>
2022-10-30 13:27:20 -0400

    Implement set_fault_status() in CTC class

--- CTC_App/Iteration3/CTC.py
+++ CTC_App/Iteration3/CTC.py
@@ -3,32 +3,31 @@ import pandas as pd
 class CTC:
     def __init__(self):
         self.__track_dictionary  =	{
-                                    "track_id": [], #
-                                    "track_throughput": [], #
-                                    "track_block_linked_list": [], #
-                                    "track_block_fault_status": [], #
-                                    "track_block_maintenance_status": [], #
-                                    "track_block_length": [], #
-                                    "track_block_speed_limit": [], #
-                                    "track_block_suggested_speed": [], #
-                                    "track_block_switches": [], #
-                                    "track_block_crossing": [], #
-                                    "track_block_stations": [], #
-                                    "track_block_occupancy": [], #
-                                    "track_block_authority": [] #
+                                    "track_id_external": [], 
+                                    "track_throughput": [], 
+                                    "track_block_linked_list": [], 
+                                    "track_block_fault_status": [], 
+                                    "track_block_maintenance_status": [], 
+                                    "track_block_length": [], 
+                                    "track_block_speed_limit": [], 
+                                    "track_block_suggested_speed": [], 
+                                    "track_block_switches": [],
+                                    "track_block_crossing": [],
+                                    "track_block_stations": [], 
+                                    "track_block_occupancy": [], 
+                                    "track_block_authority": [] 
                                     }
 
         self.__train_dictionary = {}
         self.__manula_mode_flag = False
-        self.__rack_id_counter = 0
 
     def add_track(self, file_path):
         # load track layout
         df_track_layout = pd.read_excel(file_path)
 
         # set track id and update track id counter
-        self.__track_dictionary["track_id"].append(self.__rack_id_counter)
-        self.__rack_id_counter += 1
+        line_num = df_track_layout.loc[:,"Line"]
+        self.__track_dictionary["track_id_external"].append(line_num[0])
 
         # set throughput of track to 0
         self.__track_dictionary["track_throughput"].append(0)
@@ -40,7 +39,7 @@ class CTC:
         block_switches = []
         for i in range(0, len(all_blocks)):
             if switches_1[i] == 0:
-                block_switches.append(0)
+                block_switches.append(None)
             else:
                 block_switches.append([switches_1[i], switches_2[i]])
         self.__track_dictionary["track_block_switches"].append(block_switches)
@@ -49,7 +48,7 @@ class CTC:
         block_linked = {}
         end_block = df_track_layout.loc[:,"End"]
         for i in range(0, len(all_blocks)):
-            if switches_1[i] == 0:
+            if switches_1[i] == None:
                 if end_block[i] == 1:
                     block_linked[all_blocks[i]] = None
                 else:
@@ -76,14 +75,14 @@ class CTC:
             block_length_dict[all_blocks[i]] = block_length[i]
             block_speed_limit_dict[all_blocks[i]] = block_speed_limit[i]
             block_suggest_speed_dict[all_blocks[i]] = block_speed_limit[i]
-            block_fault_status_dict[all_blocks[i]] = 0
-            block_maintenance_status_dict[all_blocks[i]] = 0
-            block_occpuancy_dict[all_blocks[i]] = 0
-            block_authority_dict[all_blocks[i]] = 1
+            block_fault_status_dict[all_blocks[i]] = False
+            block_maintenance_status_dict[all_blocks[i]] = False
+            block_occpuancy_dict[all_blocks[i]] = False
+            block_authority_dict[all_blocks[i]] = True
             if block_crossing[i] == 0:
                 block_crossing_dict[all_blocks[i]] = None
             else:
-                block_crossing_dict[all_blocks[i]] = 0
+                block_crossing_dict[all_blocks[i]] = False
             if block_station[i] == 0:
                 block_station_dict[all_blocks[i]] = None
             else:
@@ -99,10 +98,36 @@ class CTC:
         self.__track_dictionary["track_block_crossing"].append(block_crossing_dict)
         self.__track_dictionary["track_block_stations"].append(block_station_dict)
 
-    
+    # input is list of tuples -> [(x0,y0),(x1,y1),...,(xn,yn)] -> [(1001, 0), (1002, 1),...]
+    def set_fault_status(self, block_bool):
+        # get fault status list from dictionary
+        fault_status_list = self.__track_dictionary["track_block_fault_status"]
+        authority_list = self.__track_dictionary["track_block_authority"]
+        for i in range(0, len(block_bool)):
+            curr_block = block_bool[i]
+            line_num = (curr_block[0]-(curr_block[0]%1000))/1000 # translate line num (2000 + xyz -> 2)
+            block_num = curr_block[0] - (line_num*1000)
+            block_status = curr_block[1]
+
+            # get line and block index
+            line_index = self.__track_dictionary["track_id_external"].index(line_num)
+            # update status using indices
+            fault_status_list[line_index][block_num] = block_status
+            # update authority
+            if block_status == True:
+                authority_list[line_index][block_num] = False
+        
+        # update fault status and authority list in dictionary
+        self.__track_dictionary["track_block_fault_status"] = fault_status_list
+        self.__track_dictionary["track_block_authority"] = authority_list
+
+    # def get_authority(self, block_bool):
+
+
 
 ctc_test = CTC()
-CTC.add_track(ctc_test, "Iteration3/Track_Layout_Blue.xlsx")
+ctc_test.add_track("Iteration3/Track_Layout_Blue.xlsx")
+ctc_test.set_fault_status([(1003, True)])
 
 
         
