2f9b409ed6b5d54f50aea59517dbf0fc9eeb7084
jsb19227 <45880580+jsb19227@users.noreply.github.com>
2022-11-03 19:57:16 -0400

    Add files via upload

--- TrainController/input/passengerInput.py
+++ TrainController/input/passengerInput.py
@@ -3,4 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class PassengerInput:
-    emergencyBrake: bool = False
+    emergency_brake: bool = False
--- TrainController/input/trainDriverInput.py
+++ TrainController/input/trainDriverInput.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class TrainDriverInput:
-    commandSetPoint: float = 0.0
-    serviceBrake: bool = False
-    emergencyBrake: bool = False
-    manualMode: bool = False
-    interiorTemperatureControl: int = 70
-    insideLights: bool = False
-    outsideLights: bool = False
-    rightSideDoors: bool = False
-    leftSideDoors: bool = False
-    activateAnnouncement: bool = False
+    command_set_point: float = 0.0
+    service_brake: bool = False
+    emergency_brake: bool = False
+    manual_mode: bool = False
+    interior_temperature_control: int = 70
+    inside_lights: bool = False
+    outside_lights: bool = False
+    right_side_doors: bool = False
+    left_side_doors: bool = False
+    activate_announcement: bool = False
