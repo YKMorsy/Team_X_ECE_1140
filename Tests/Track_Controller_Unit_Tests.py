5e0829e660283f12f271593824280640d3d4adfb
SSkyeG <60981354+SSkyeG@users.noreply.github.com>
2022-12-06 23:02:17 -0500

    Initialize Waysides with excel and right speed info
    classic spending more time to make a fancy way to do something rather than doing it by hand

--- Tests/Track_Controller_Unit_Tests.py
+++ Tests/Track_Controller_Unit_Tests.py
@@ -1,6 +1,23 @@
 import unittest
 #from track_controller import WaysideController
-import ..track_controller 
+import sys
+import os
+ 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+ 
+# now we can import the module in the parent
+# directory.
+from track_controller.track_controller import WaysideController 
 
 class TestTrackController(unittest.TestCase):
 
