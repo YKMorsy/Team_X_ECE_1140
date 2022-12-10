8270b7f1b058cf2f00d4f98dbce05061ff6bdaa8
Peter Smith <77897203+p214smith@users.noreply.github.com>
2022-12-10 14:40:07 -0500

    track updates

--- connect_ctc_track_model.py
+++ connect_ctc_track_model.py
@@ -3,7 +3,7 @@ def connect_ctc_track_model(green_line, red_line, track_model_var):
     maintenance_green = green_line.maintenance_blocks
 
     for block in maintenance_green:
-        track_model_var.clear_block_failure(int(block))
+        track_model_var.clear_block_failure(int(block)-1)
 
     green_line.maintenance_blocks = []
 
